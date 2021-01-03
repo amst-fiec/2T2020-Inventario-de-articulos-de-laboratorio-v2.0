bb4558b6afd5e27f9f07726d43df098afffab6f6
cquisay <cquisay@espol.edu.ec>
2021-01-03 18:50:48 -0500

    Formulario para ingresar laboratorio
    Solo el formulario (parte visual)

---
+++ app/src/main/java/com/example/labstock/registro_laboratorio.java
@@ -0,0 +1,14 @@
+package com.example.labstock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registro_laboratorio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_laboratorio);
+    }
+}
