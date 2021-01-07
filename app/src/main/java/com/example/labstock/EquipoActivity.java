0822cf1987361dee6dd2966742dc12bcbface2ce
lopez damian <dennyslopez2013@gmail.com>
2021-01-06 21:21:45 -0500

    config prestamo

--- app/src/main/java/com/example/labstock/EquipoActivity.java
+++ app/src/main/java/com/example/labstock/EquipoActivity.java
@@ -2,13 +2,25 @@ package com.example.labstock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EquipoActivity extends AppCompatActivity {
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipo);
+        context = getApplicationContext();
+    }
+
+    public void prestarEquipo(View view) {
+
+        Intent intent = new Intent(context, PrestarEquipoActivity.class);
+        startActivity(intent);
     }
 }
